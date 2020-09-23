33ba0b1ae2bbff36561fa2cfcfb47e7d51d0b2f4
carloscacho <carlosemilio.ac@gmail.com>
2020-09-22 21:08:40 -0400

    finalização da rede neural

--- deeplib/nn.py
+++ deeplib/nn.py
@@ -7,7 +7,22 @@ Uma rede neural é apenas uma coleção de camadas (layers).
 Ela se comporta muito como uma camada(layer), embora
 não vamos fazer novamente uma camada
 """
+from typing import Sequence
 
 from deeplib.tensor import Tensor
 from deeplib.layers import Layer
+
+class NeuralNet:
+    def __init__(self, layers: Sequence[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        for layer in self.layers:
+            inputs = layer.forward(inputs)
+        return inputs
+    
+    def backward(self, grad: Tensor) -> Tensor:
+        for layer in reversed(self.layers):
+            grad = layer.backward(grad)
+        return grad
 
