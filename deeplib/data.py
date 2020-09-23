ca960744744027e38c803005ffd70df0424590e4
carloscacho <carlosemilio.ac@gmail.com>
2020-09-22 21:46:20 -0400

    crição do data para geração de lotes de dados

---
+++ deeplib/data.py
@@ -0,0 +1,33 @@
+"""
+we'll feed inputs into out network in batches.
+So here are some tools for iterating over data in batches.
+
+iremos alimentar as entradas em nossa rede neurais(NeuralNet) em lotes.
+Portanto, aqui estão algumas ferramentas para iterar os dados em lotes.
+"""
+from typing import Iterator, NamedTuple
+import numpy as np
+
+from deeplib.tensor import Tensor 
+
+Batch = NamedTuple("Batch", [("inputs", Tensor), ("targets", Tensor)])
+
+class DataIterator:
+    def __call__(self, inputs: Tensor, targets: Tensor) -> Iterator[Batch]:
+        raise NotImplementedError
+
+class BatchIterator:
+    def __init__(self, batch_size: int = 32, shuffle: bool = True) -> None:
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+    def __call__(self, inputs: Tensor, targets: Tensor) -> Iterator[Batch]:
+        starts = np.arange(0, len(inputs), self.batch_size)
+        if self.shuffle:
+            np.random.shuffle(starts)
+        
+        for start in starts:
+            end = start + self.batch_size
+            batch_inputs = inputs[start:end]
+            batch_targets = targets[start:end]
+            yield Batch(batch_inputs, batch_targets)
